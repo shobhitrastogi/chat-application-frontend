c910fe75194e23fb79172b7445612cec5554a848
shobhitrastogi <rshobhit255@gmail.com>
2023-12-15 21:35:25 +0530

    Changes in URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 import {io} from 'socket.io-client';
 
 import Chat from './Chat';
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://chat-application-d59t.onrender.com/');
+
 
 function App() {
 const [userName,setUserName]=useState("")
